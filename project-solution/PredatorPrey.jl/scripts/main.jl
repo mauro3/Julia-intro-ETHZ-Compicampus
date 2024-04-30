3c57f8b7a560c59d3d9a14964939ccf112b6b25b
Ivan Utkin <iutkin@ethz.ch>
2024-04-30 16:07:15 +0200

    Add content

--- project-solution/PredatorPrey.jl/scripts/main.jl
+++ project-solution/PredatorPrey.jl/scripts/main.jl
@@ -16,22 +16,33 @@ nt = 1000
 x0 = (10.0, 10.0)
 
 # allocate arrays
-out1 = zeros(nt, 2)
-out2 = zeros(nt, 2)
+out       = zeros(nt, 2)
+out_naive = zeros(nt, 2)
+out_fancy = zeros(nt, 2)
 
-# integrate ODE
-t1, out1 = integrate_naive!(out1, x0, α, β, δ, γ, Δt, nt)
-t2, out2 = integrate_fancy!(out2, x0, α, β, δ, γ, Δt, nt)
+# solve ODEs in global scope
+out[1, :] .= x0
+@time for it in 2:nt
+    out[it, :] .= out[it-1, :] .+ Δt .* predator_prey(out[it-1, :], α, β, δ, γ)
+end
+t = range(0, (nt - 1) * Δt, nt)
 
 # visualise results
-p1 = plot(t1, out1; ylabel="population", label=["prey" "predator"], title="explicit Euler", ls=[:solid :dash], lw=1.5)
-p2 = plot(out1[:, 1], out1[:, 2]; ylabel="predator", title="phase plot", lw=1.5, label=false)
+plot(t, out; ylabel="population", label=["prey" "predator"], ls=[:solid :dash], lw=1.5)
 
-p3 = plot(t2, out2; xlabel="time", ylabel="population", label=["prey" "predator"], title="semi-implicit Euler", ls=[:solid :dash], lw=1.5)
-p4 = plot(out2[:, 1], out2[:, 2]; xlabel="prey", ylabel="predator", title="phase plot", lw=1.5, label=false)
+# integrate ODEs using functions
+@time t_naive, out_naive = integrate_naive!(out_naive, x0, α, β, δ, γ, Δt, nt)
+@time t_fancy, out_fancy = integrate_fancy!(out_fancy, x0, α, β, δ, γ, Δt, nt)
 
-display(plot(p1, p2, p3, p4; layout=(2, 2), size=(640, 640)))
+# visualise results
+p1 = plot(t_naive, out_naive; ylabel="population", label=["prey" "predator"], title="explicit Euler", ls=[:solid :dash], lw=1.5)
+p2 = plot(out_naive[:, 1], out_naive[:, 2]; ylabel="predator", title="phase plot", lw=1.5, label=false)
+
+p3 = plot(t_fancy, out_fancy; xlabel="time", ylabel="population", label=["prey" "predator"], ls=[:solid :dash], lw=1.5)
+p4 = plot(out_fancy[:, 1], out_fancy[:, 2]; xlabel="prey", ylabel="predator", title="phase plot", lw=1.5, label=false)
+
+plot(p1, p2, p3, p4; layout=(2, 2), size=(640, 640))
 
 # benchmark implementations
-@benchmark integrate_naive!($out1, $x0, $α, $β, $δ, $γ, $Δt, $nt)
-@benchmark integrate_fancy!($out2, $x0, $α, $β, $δ, $γ, $Δt, $nt)
+@benchmark integrate_naive!($out_naive, $x0, $α, $β, $δ, $γ, $Δt, $nt)
+@benchmark integrate_fancy!($out_fancy, $x0, $α, $β, $δ, $γ, $Δt, $nt)
--- project/PredatorPrey.jl/scripts/main.jl
+++ project/PredatorPrey.jl/scripts/main.jl
@@ -9,19 +9,29 @@ using BenchmarkTools
 γ = 0.4
 
 # numerics
-Δt = 0.1
-nt = 1000
+Δt = 0.01
+nt = 10000
 
 # initial condition
 x0 = (10.0, 10.0)
 
-# allocate arrays
-out1 = zeros(nt, 2)
-out2 = zeros(nt, 2)
+out       = zeros(nt, 2)
+out_naive = zeros(nt, 2)
+out_fancy = zeros(nt, 2)
 
-# integrate ODE
-t1, out1 = integrate_naive!(...)
-t2, out2 = integrate_fancy!(...)
+# solve ODEs in global scope
+out[1, :] .= x0
+@time for it in 2:nt
+    out[it, :] .= out[it-1, :] .+ ...
+end
+t = range(0, ..., nt)
+
+# visualise results
+plot(...; ylabel="population", label=["prey" "predator"], ls=[:solid :dash], lw=1.5)
+
+# integrate ODEs using functions
+@time t_naive, out_naive = integrate_naive!(...)
+@time t_fancy, out_fancy = integrate_fancy!(...)
 
 # visualise results
 p1 = plot(...; ylabel="population", label=["prey" "predator"], title="explicit Euler", ls=[:solid :dash], lw=1.5)
